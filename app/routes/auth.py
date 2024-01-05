1737b1d1b8db8303616515e2d54b50d9f488ae3f
EmmanuelOZ <davidozuna84@hotmail.com>
2024-01-04 20:23:09 -0600

    iniciar sesion y prueba de ruta protegida implementada

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, make_response, session
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 from app.models import UserLogin, UserModel
 from app.services.auth import post_login_api
 
@@ -8,6 +8,9 @@ auth_bp = Blueprint('AuthRoute', __name__)
 
 @auth_bp.get('/login')
 def get_login():
+    if current_user.is_authenticated:
+        return redirect(url_for('IndexRoute.index'))
+
     user_form = UserLogin()
     context = {'form': user_form, 'title': 'Login'}
     return render_template('auth.jinja', **context)
@@ -18,14 +21,14 @@ def post_login():
     user_form = UserLogin()
     if not user_form.validate_on_submit():
         print(user_form.errors)
-        flash('Error en el formulario')
+        flash('Error en el formulario', 'error')
         return redirect(url_for('AuthRoute.get_login'))
 
     response, error = post_login_api(user_form.data)
 
     if error is not None:
-        print(error)
-        flash(error)
+        flash(error, 'error')
+        print(response.json())
         return redirect(url_for('AuthRoute.get_login'))
 
     data = response.json()
@@ -35,8 +38,9 @@ def post_login():
     user = res["user"]
 
     session["user"] = user
-    print(user)
+
     user = UserModel(user)
+
     login_user(user)
 
     response = make_response(redirect(url_for('IndexRoute.index')))
@@ -48,8 +52,12 @@ def post_login():
 
 @auth_bp.get('/logout')
 def get_logout():
-    # Lógica para cerrar sesión
-    pass
+    logout_user()
+    session.clear()
+    # limpiar cookies
+    response = make_response(redirect(url_for('AuthRoute.get_login')))
+    response.set_cookie('access_token', '')
+    return response
 
 
 @auth_bp.get('/signup')
--- app/services/auth.py
+++ app/services/auth.py
@@ -4,10 +4,20 @@ URL_BASE = Config.URL_API + '/auth'
 
 
 def post_login_api(user_form):
-    url = f'{URL_BASE}/login'
-    response = requests.post(url, json=user_form)
+    try:
+        url = f'{URL_BASE}/login'
+        response = requests.post(url, json=user_form)
 
-    if response.status_code != 200:
-        return response, response.json()['msg']
+        if response.status_code != 200:
+            return response, response.json()['msg']
 
-    return response, None
+        if not response.json():
+            return response, "Error en el servidor"
+
+        if response.json()['status'] == 'error':
+            return response, response.json()['msg']
+
+        return response, None
+    except Exception as e:
+        print(e)
+        return None, "Error en el servidor"
